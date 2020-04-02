9a214170763e6fdd73d17fc77afb04b65d215e52
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-04-02 00:07:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,54 @@
 class Triangle
-  # write code here
+  
+
+  attr_accessor :side1, :side2, :side3
+  @sides = []
+
+  def initialize(side1, side2, side3)
+    @side1, @side2, @side3 = side1, side2, side3
+    @sides = [side1, side2, side3]
+  end
+
+  def kind
+    if(@side1 == 0 || @side2 == 0 || @side3 == 0)
+      elsif()
+      raise TriangleError
+    elsif(equilateral?)
+      :equilateral
+    elsif(scalene?)
+      :scalene
+    elsif(isosceles?)
+      :isosceles
+    end
+  end
+  
+  def negative?
+    @sides.each do |length|
+      if(length <= 0)
+        return true
+      end
+    end
+  end
+
+  def equilateral?
+    (@side1 == @side2) && (@side1 == @side3)
+  end
+
+  def scalene?
+    (@side1 != @side2) && (@side1!= @side3) && (@side2 != @side3)
+  end
+
+  def isosceles?
+    (@side2 == @side3) || (@side1 == @side2) || (@side1 = @side3)
+  end
+
+  def valid?
+    (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Not a triangle!"
+    end
+  end
 end
